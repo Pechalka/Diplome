ab09910e4687177ef892220181f467b08ba25697
Andrey <luzkovskyandrey@gmail.com>
2012-03-21 01:46:14 +0200

    added company detais

--- Diplome/Diplome/Controllers/CompaniesController.cs
+++ Diplome/Diplome/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using Diplome.Models;
 
@@ -6,13 +7,20 @@ namespace Diplome.Controllers
 {
     public class CompaniesController : ApiController
     {
-        public IEnumerable<Company> Get()
-        {
-            return new List<Company>
+        private readonly List<Company> companies = new List<Company>
                        {
                            new Company{ Id = 2, Name = "Надежда", Description = "Парихмахерская"},
                            new Company{ Id = 1, Name = "MMM", Description = "Финансовая перамида"}
                        };
+
+        public IEnumerable<Company> Get()
+        {
+            return companies;
+        }
+
+        public Company Get(int id)
+        {
+            return companies.FirstOrDefault(c => c.Id == id);
         }
     }
 }
--- Diplome/Diplome/Scripts/App/Main.js
+++ Diplome/Diplome/Scripts/App/Main.js
@@ -1,50 +1,64 @@
-﻿
-window.Company = Backbone.Model.extend();
-
-window.Companies = Backbone.Collection.extend({
-    model: Company,
-    url: "/api/Companies"
-});
-
-
-
-window.CompaniesList = Backbone.View.extend({
-    initialize: function () {
-        this.model.bind("reset", this.render, this);
-    },
-    render: function () {
-        var template = _.template($('#tpl-company-item').html());
-        var container = $(this.el);
-        this.model.each(function (company) {
-            var html = template(company.toJSON());
-            container.append(html);
+﻿$(function () {
+    window.Company = Backbone.Model.extend({
+            idAttribute : 'Id'
         });
-        return this;
-    }
-});
+
+    window.Companies = Backbone.Collection.extend({
+        model: Company,
+        url: "/api/Companies"
+    });
 
 
-// Router
-var AppRouter = Backbone.Router.extend({
 
-    routes: {
-        "": "CompanysList",
-        "wines/:id": "CompanyDetails"
-    },
+    window.CompaniesList = Backbone.View.extend({
+        initialize: function () {
+            this.model.bind("reset", this.render, this);
+        },
+        render: function () {
+            var template = _.template($('#tpl-company-item').html());
+            var container = $(this.el);
+            this.model.each(function (company) {
+                var html = template(company.toJSON());
+                container.append(html);
+            });
+            return this;
+        }
+    });
 
-    CompanysList: function () {
-        var companies = new Companies();
+    window.CompanyDetails = Backbone.View.extend({
+        render: function () {
+            var template = _.template($('#tpl-company-details').html());
+            $(this.el).html(template(this.model.toJSON()));
+            return this;
+        }
+    });
 
-        var view = new CompaniesList({ model: companies, el: $('#content') });
 
-        companies.fetch();
-    },
-    CompanyDetails: function (id) {
+    // Router
+    var AppRouter = Backbone.Router.extend({
 
-    }
-});
+        routes: {
+            "": "CompanysList",
+            "Companys/:id": "CompanyDetails"
+        },
 
-$(function() {
+        CompanysList: function () {
+            this.companies = new Companies();
+
+            var view = new CompaniesList({ model: this.companies });
+
+            this.companies.fetch();
+
+            $('#content').html(view.render().el);
+        },
+        CompanyDetails: function (id) {
+            var company = this.companies.get(id);
+            var view = new CompanyDetails({ model: company });
+            $('#content').html(view.render().el);
+        }
+    });
+
+
     var app = new AppRouter();
     Backbone.history.start();
 });
